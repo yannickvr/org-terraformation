b4739aed4481ec2ccfe315458fe74e4b44a5803f
Yannick van Rooyen <yannick@yannickvr.nl>
2023-04-17 12:09:15 +0200

    Add some descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,10 @@ output "accounts" {
   value = {
     for k, v in aws_organizations_account.account : k => v.id
   }
+  description = "Account names and account IDs"
 }
 
 output "ous" {
   value = local.ous
+  description = "Organizational units and OU Ids"
 }
